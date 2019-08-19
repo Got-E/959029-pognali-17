45549229aa16299cde16fac296d17def5b1f6c1a
Got-E <i.mail.i@mail.ru>
2019-08-19 21:09:46 +0300

    шапка до десктопа

--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,19 @@
 var navMain = document.querySelector('.page-header__container');
 var navToggle = document.querySelector('.page-header__toggle');
+var contentMail = document.querySelector('.contacts__item--mail');
+var contentPhone = document.querySelector('.contacts__item--phone');
 
 navMain.classList.remove('page-header__container--open');
+navToggle.classList.remove('page-header__toggle--no-js')
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('page-header__container--open')) {
     navMain.classList.remove('page-header__container--open');
-
+    contentMail.classList.add('contacts__item--toggle');
+    contentPhone.classList.add('contacts__item--toggle');
   } else {
     navMain.classList.add('page-header__container--open');
-
+    contentMail.classList.remove('contacts__item--toggle');
+    contentPhone.classList.remove('contacts__item--toggle');
   }
 });
